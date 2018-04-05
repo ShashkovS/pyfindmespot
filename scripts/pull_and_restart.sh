ac7ccdbe6650fbc83e5188334449dc7209747573
Sergey Shashkov <sh57@yandex.ru>
2018-04-05 13:21:58 +0300

    Configs moved to /config

--- scripts/pull_and_restart.sh
+++ scripts/pull_and_restart.sh
@@ -5,6 +5,12 @@ echo 'Обновляем код с githubа'
 cd $repo_path
 git pull origin master
 
+echo 'Обновляем конфиги'
+cp scripts/gunicorn.service /etc/systemd/system/gunicorn.service
+cp scripts/gunicorn.socket /etc/systemd/system/gunicorn.socket
+cp scripts/gunicorn.conf  /etc/tmpfiles.d/gunicorn.conf 
+cp scripts/findmespot.conf /etc/nginx/conf.d/findmespot.conf
+
 echo 'Перезапускаем всё'
 systemctl stop gunicorn.socket
 systemctl start gunicorn.socket
