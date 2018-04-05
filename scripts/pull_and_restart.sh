4797b63dc77a79478852ed78eff48a5b9c67ff4d
Sergey Shashkov <sh57@yandex.ru>
2018-04-05 13:33:44 +0300

    test

--- scripts/pull_and_restart.sh
+++ scripts/pull_and_restart.sh
@@ -6,10 +6,10 @@ cd $repo_path
 git pull origin master
 
 echo 'Обновляем конфиги'
-cp scripts/gunicorn.service /etc/systemd/system/gunicorn.service
-cp scripts/gunicorn.socket /etc/systemd/system/gunicorn.socket
-cp scripts/gunicorn.conf  /etc/tmpfiles.d/gunicorn.conf 
-cp scripts/findmespot.conf /etc/nginx/conf.d/findmespot.conf
+cp -u config/gunicorn.service /etc/systemd/system/gunicorn.service
+cp -u config/gunicorn.socket /etc/systemd/system/gunicorn.socket
+cp -u config/gunicorn.conf  /etc/tmpfiles.d/gunicorn.conf 
+cp -u config/findmespot.conf /etc/nginx/conf.d/findmespot.conf
 
 echo 'Перезапускаем всё'
 systemctl stop gunicorn.socket
