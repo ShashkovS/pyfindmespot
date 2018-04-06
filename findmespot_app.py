75b5ad40f4e5d04c2f04d54dc378eaeef36d5271
Виктор <viktor@Viktors-MacBook-Air.local>
2018-04-06 21:51:23 +0300

    printing args

--- findmespot_app.py
+++ findmespot_app.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from flask import request
 from werkzeug.contrib.fixers import ProxyFix  # For Gunicorn
 
 app = Flask(__name__)
 app.config["APPLICATION_ROOT"] = "/findmespot"
 # @app.route("/")
 
+
 @app.route('/', defaults={'path': ''})  # Это — хук для того, чтобы обрабатывать пустой адрес и передавать в параметр пустой путь
 @app.route('/<path:path>')  # Это — хук для того, чтобы обрабатывать все адреса и передавать в параметр запрошенный путь
 def hello(path):
-    return """<h1>Flask app really works here!</h1><p>Path is: """ + path + """."""
+    some_args = request.args
+    return str(some_args)
 
 
 app.wsgi_app = ProxyFix(app.wsgi_app)  # For Gunicorn
