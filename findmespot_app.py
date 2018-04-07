cc179b85431c1075a4b1dd4c601717c6b0ead558
Eyakm <abw00001@ya.ru>
2018-04-07 10:21:13 +0300

    Result packed in JSON + added some API funcs

--- findmespot_app.py
+++ findmespot_app.py
@@ -1,19 +1,47 @@
 from flask import Flask
+from flask import jsonify
 from flask import request
-from werkzeug.contrib.fixers import ProxyFix  # For Gunicorn
+import werkzeug.contrib.fixers
+import werkzeug.exceptions
 
 app = Flask(__name__)
 app.config["APPLICATION_ROOT"] = "/findmespot"
 # @app.route("/")
 
 
+@app.errorhandler(werkzeug.exceptions.InternalServerError)
+def internal_error_handler(e):
+    message = {
+        'status': 500,
+        'message': 'API method not found: ' + request.url,
+        'return': {}
+    }
+    response = jsonify(message)
+    response.status_code = 500
+    return response
+
+
+def test(args):
+    message ={
+        'status': 200,
+        'message': 'OK',
+        'return': dict(args)
+    }
+    response = jsonify(message)
+    response.status_code = 200
+    return response
+
+
 @app.route('/', defaults={'path': ''})  # Это — хук для того, чтобы обрабатывать пустой адрес и передавать в параметр пустой путь
 @app.route('/<path:path>')  # Это — хук для того, чтобы обрабатывать все адреса и передавать в параметр запрошенный путь
 def hello(path):
-    some_args = request.args
-    return str(some_args)
+    args = request.args
+    if path == '':
+        return jsonify('It works!')
+    else:
+        return eval('{}({})'.format(path, dict(args)))
 
 
-app.wsgi_app = ProxyFix(app.wsgi_app)  # For Gunicorn
+app.wsgi_app = werkzeug.contrib.fixers.ProxyFix(app.wsgi_app)  # For Gunicorn
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
