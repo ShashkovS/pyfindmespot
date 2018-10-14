a796a60d5ebbacfb4a730ee52aba71c11a69707a
eyakm1 <abw00001@yandex.ru>
2018-10-14 19:32:41 +0300

    Added generating gpx files

--- findmespot_app.py
+++ findmespot_app.py
@@ -1,12 +1,13 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, Response
 from prefix_and_wsgi_proxy_fix import ReverseProxied
+import datetime
+import gpxpy.gpx
 import geojson
 import werkzeug.exceptions
 import sqlite3
 
-
 app = Flask(__name__)
-DB_DEFAULT_PATH = 'db/tracks.db'
+DB_DEFAULT_PATH = r'db\tracks.db'
 
 
 # This is just a test route. It is autotested after deploy
@@ -48,14 +49,14 @@ def get_waypoints(*args, **kwargs):
         trip_name = dict(request.args)['trip_name'][0]
         table = cur.execute('''SELECT * FROM trips where name = ?''', (trip_name,))
         name, date_s, date_e, fms_id = table.fetchall()[0]
-        waypoints = cur.execute('''SELECT * FROM waypoints where fms_key_id = ?''', (str(fms_id), )).fetchall()
+        waypoints = cur.execute('''SELECT * FROM waypoints where fms_key_id = ?''', (str(fms_id),)).fetchall()
         features = []
         for i in range(len(waypoints)):
             id, fms_key_id, id_fms, lat, long, alt, ts, bs, msg = waypoints[i]
             cur_point = geojson.Point((lat, long, alt))
             features.append(geojson.Feature(geometry=cur_point, properties={'BatteryState': bs,
-                                                                          'Message': msg,
-                                                                          'TimeStamp': ts}))
+                                                                            'Message': msg,
+                                                                            'TimeStamp': ts}))
         message = {
             'status': 200,
             'message': 'OK',
@@ -65,6 +66,31 @@ def get_waypoints(*args, **kwargs):
         response = jsonify(message)
         response.status_code = 200
         return response
+
+
+@app.route('/get_gpx_waypoints')
+def generate_gpx(*args, **kwargs):
+    gpx = gpxpy.gpx.GPX()
+    gpx_track = gpxpy.gpx.GPXTrack()
+    gpx.tracks.append(gpx_track)
+    gpx_segment = gpxpy.gpx.GPXTrackSegment()
+    gpx_track.segments.append(gpx_segment)
+    with sqlite3.connect(DB_DEFAULT_PATH) as con:
+        cur = con.cursor()
+        if 'trip_name' not in dict(request.args):
+            return bad_request_error_handler(NameError(f'Key trip_name not found'))
+        trip_name = dict(request.args)['trip_name'][0]
+        table = cur.execute('''SELECT * FROM trips where name = ?''', (trip_name,))
+        name, date_s, date_e, fms_id = table.fetchall()[0]
+        waypoints = cur.execute('''SELECT * FROM waypoints where fms_key_id = ?''', (str(fms_id),)).fetchall()
+        features = []
+        for i in range(len(waypoints)):
+            id, fms_key_id, id_fms, lat, long, alt, ts, bs, msg = waypoints[i]
+            ts_time = datetime.datetime.strptime(ts, "%Y-%m-%dT%H:%M:%SZ")
+            cur_pnt = gpxpy.gpx.GPXTrackPoint(latitude=lat, longitude=long, elevation=alt, comment=msg, time=ts_time)
+            cur_pnt.description = f"Время: {ts}\n Заряд батареи {bs}\n"
+            gpx_segment.points.append(cur_pnt)
+    return Response(gpx.to_xml(), mimetype="text/xml")
 
 
 @app.route('/')
