2387a57d59cb9c4e1ef18398a2755ad0f03e1130
Sergey Shashkov <sh57@yandex.ru>
2019-03-17 22:30:31 +0300

    add cgi config

---
+++ cgi-bin/github_commit_webhook.py
@@ -0,0 +1,7 @@
+#!/web/pyfindmespot/pyfindmespot_env/bin/python
+# -*- coding: utf-8 -*-
+
+print("HTTP/1.0 200 OK")
+print("Content-Type: text/html\n")
+print("")
+print("<!doctype html><title>Hello</title><h2>hello world</h2>")
